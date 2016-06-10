c275ef2cf315d2596f4eabdc8336b1639574b5a2
dschrag <dschrag@purdue.edu>
2016-06-09 21:18:25 -0400

    Bus data is now available.

--- IndyGo/platforms/browser/www/js/gtfsparser.js
+++ IndyGo/platforms/browser/www/js/gtfsparser.js
@@ -1,14 +1,50 @@
 function readDropbox(sURL)
 {
 		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open('GET', sURL, true);
+		xmlhttp.open('GET', sURL, false);
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.readyState == 4) {
 				if(xmlhttp.status == 200) {
-          console.log(xmlhttp.responseText);
+          //console.log(xmlhttp.responseText);
+          parseGTFS(xmlhttp.responseText);
 				}
 			}
 		};
 		xmlhttp.send(null);
-	
+
+}
+
+function Trip(trip_id, lat, lng, time, vehicle) {
+    this.trip_id = trip_id;
+    this.pos = {lat: lat, lng: lng}
+    this.timestamp = time;
+    this.vehicle = vehicle;
+}
+
+function parseGTFS(response) {
+    var strarr = response.split("\n"); // remove spaces
+    console.log(strarr);
+    var i = 0, k = 0;
+
+    console.log(strarr[i+1].split("\"")[1]); // gets trip_id
+    console.log(strarr[i+4].replace('\"', '').split(':')[1].replace(' ', '')); // gets latitude
+    console.log(strarr[i+5].replace('\"', '').split(':')[1].replace(' ', '')); // gets longitude
+    console.log(strarr[i+7].split(':')[1].replace(' ', '')); // gets timestamp
+    console.log(strarr[i+9].split(' ')[3].replace("\"", '').replace("\"", '')); // gets vehicle label
+
+    var trip_array = [];
+
+    while (i+10 < strarr.length) {
+          var trip_id = strarr[i+1].split("\"")[1];
+          var lat = strarr[i+4].replace('\"', '').split(':')[1].replace(' ', '');
+          var lng = strarr[i+5].replace('\"', '').split(':')[1].replace(' ', '');
+          var time = strarr[i+7].split(':')[1].replace(' ', '');
+          var vehicle = strarr[i+9].split(' ')[3].replace("\"", '').replace("\"", '');
+
+         trip_array[k++] = new Trip(trip_id, lat, lng, time, vehicle);
+
+         i+=11;
+     }
+
+     console.log(trip_array);
 }
