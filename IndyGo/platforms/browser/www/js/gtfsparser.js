ca7069846c8df6bae059818829dfed7db2c085e9
dschrag <dschrag@purdue.edu>
2016-06-10 21:14:38 -0400

    I got buses to do things

--- IndyGo/platforms/browser/www/js/gtfsparser.js
+++ IndyGo/platforms/browser/www/js/gtfsparser.js
@@ -1,36 +1,36 @@
 function readDropbox(sURL)
 {
 		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open('GET', sURL, false);
+		xmlhttp.open('GET', sURL, true);
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.readyState == 4) {
 				if(xmlhttp.status == 200) {
-          //console.log(xmlhttp.responseText);
-          parseGTFS(xmlhttp.responseText);
+          console.log(xmlhttp.responseText);
+					var x = parseGTFS(xmlhttp.responseText);
+					return x;
 				}
 			}
 		};
 		xmlhttp.send(null);
-
 }
 
 function Trip(trip_id, lat, lng, time, vehicle) {
     this.trip_id = trip_id;
-    this.pos = {lat: lat, lng: lng}
+    this.pos = {lat: Number(lat), lng: Number(lng)};
     this.timestamp = time;
     this.vehicle = vehicle;
 }
 
 function parseGTFS(response) {
     var strarr = response.split("\n"); // remove spaces
-    console.log(strarr);
+    // console.log(strarr);
     var i = 0, k = 0;
 
-    console.log(strarr[i+1].split("\"")[1]); // gets trip_id
-    console.log(strarr[i+4].replace('\"', '').split(':')[1].replace(' ', '')); // gets latitude
-    console.log(strarr[i+5].replace('\"', '').split(':')[1].replace(' ', '')); // gets longitude
-    console.log(strarr[i+7].split(':')[1].replace(' ', '')); // gets timestamp
-    console.log(strarr[i+9].split(' ')[3].replace("\"", '').replace("\"", '')); // gets vehicle label
+    // console.log(strarr[i+1].split("\"")[1]); // gets trip_id
+    // console.log(strarr[i+4].replace('\"', '').split(':')[1].replace(' ', '')); // gets latitude
+    // console.log(strarr[i+5].replace('\"', '').split(':')[1].replace(' ', '')); // gets longitude
+    // console.log(strarr[i+7].split(':')[1].replace(' ', '')); // gets timestamp
+    // console.log(strarr[i+9].split(' ')[3].replace("\"", '').replace("\"", '')); // gets vehicle label
 
     var trip_array = [];
 
@@ -46,5 +46,6 @@ function parseGTFS(response) {
          i+=11;
      }
 
-     console.log(trip_array);
+      console.log(trip_array);
+      return(trip_array);
 }
