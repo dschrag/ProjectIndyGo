5073ce7e23d40c6a90acf4193d211c5b8f7b2f5b
dschrag <dschrag@purdue.edu>
2016-06-13 01:07:30 -0400

    fixing bugs

--- IndyGo/platforms/browser/www/js/bikeshare.js
+++ IndyGo/platforms/browser/www/js/bikeshare.js
@@ -29,13 +29,16 @@ function createBikePins(map, stations) {
         });
         station_array[i] = marker;
 
+
         google.maps.event.addListener(marker, 'click', function () {
           if (typeof clickedStation != 'undefined') {
             clickedStation.setIcon('images/BikeMarker.png');
           }
             getBikeStatus(this);
-            map.panTo(this.position);
-            map.setCenter(this.position);
+            // map.panTo(this.position);
+            // map.setCenter(this.position);
+            console.log(this.position.lat());
+            console.log(this.position.lng());
             openInfoBox(this.getTitle(), this.addr, this.bikes_avail, this.docks_avail);
             pos = this.position;
             clickedStation = this;
@@ -67,17 +70,20 @@ function getBikeStatus(marker) {
 function getClosestBikeStation() {
   var closest = 99999;
   var closeststation = 0;
-  for (var i = 0; i < 27; i++) {
+
+  for (var i = 0; i < station_array.length; i++) {
     var distance_lat = pos.lat - station_array[i].getPosition().lat();
     var distance_lng = pos.lng - station_array[i].getPosition().lng();
     var distance = Math.sqrt(distance_lat * distance_lat + distance_lng * distance_lng);
-
     if (distance < closest) {
       closest = distance;
       closeststation = i;
     }
   }
 
+  console.log(station_array);
+  console.log(closeststation);
+  clickedStation = station_array[closeststation];
   getBikeStatus(station_array[closeststation]);
   openInfoBox(station_array[closeststation].getTitle(), station_array[closeststation].addr, station_array[closeststation].bikes_avail, station_array[closeststation].docks_avail);
 
